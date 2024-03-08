5a66e519b982141adba6590dace436a4cf72093c
NahiaraJulian <nahiarajulian@gmail.com>
2024-03-08 14:36:57 -0300

    Modified: style with Tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,18 @@
 const Footer = () => {
     return (
-      <footer>
-        <div>
-          <a href="#">Home</a>
-          <a href="#">Cities</a>
-        </div>
+      <footer className="bg-teal-500 mt-5">
+        <nav className="flex justify-center gap-11 bg-emerald-600">
+          <a href="#" className="text-white sm:text-xl">Home</a>
+          <a href="#" className="text-white sm:text-xl">Cities</a>
+        </nav>
 
-        <div>
-          <img src="/logo2.png" alt="logo white MyTinerary" className="w-14 h-14"/>
-          <h3>Contact us</h3>
-          <a href="mailto:mytinerary@travels.com">mytinerary@travels.com</a>
+        <div className="flex items-center h-28 relative pb-6 pl-6">
+          <img src="/logo2.png" alt="logo white MyTinerary" className="w-14 h-14 object-cover"/>
+          <section className="flex flex-col pl-3">
+            <h3 className="text-2xl text-white font-semibold">Contact us</h3>
+            <a href="mailto:mytinerary@travels.com" className="text-xl text-white">mytinerary@travels.com</a>
+          </section>
+          <p className="text-emerald-800 absolute bottom-0 right-0 pr-1 pb-1 text-sm sm:text-base">Copyright © - MyTinerary - All rights reserved</p>
         </div>
       </footer>
     )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 const Hero = (props) => {
     return (
-      <div>
-        <img src={props.banner} alt="banner travel" className='w-full h-screen object-cover'/>
-        <img src="/logo.png" alt="Logo My Tinerary" className='w-14 h-14'/>
-        <h1>My Tinerary</h1>
-        <p>Find your perfect trip, designed by insiders who know and love their cities!</p>
+      <div className="flex justify-center items-center relative w-full h-[60vh] sm:h-[80vh] mb-5">
+        <img src={props.banner} alt="banner" className='w-full h-full object-cover'/>
+
+        <section className="absolute flex flex-wrap justify-center items-center gap-3 px-3">
+          <img src="/logo.png" alt="Logo My Tinerary" className='h-14 sm:h-16 md:h-20 lg:h-32'/>
+          <h1 className="font-bold text-emerald-600 text-5xl sm:text-7xl md:text-8xl lg:text-9xl">MyTinerary</h1>
+          <p className="text-orange-500 font-semibold pt-7 text-sm w-9/12 text-center sm:text-lg md:text-2xl">Find your perfect trip, designed by insiders who know and love their cities!</p>
+        </section>
       </div>
     )
 }
